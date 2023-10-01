dd4dceaf6450cb59a0038b5ec25d432cb6f07fc4
Bekzat <116498386+alisonavi@users.noreply.github.com>
2023-10-01 23:51:32 +0500

    Add files via upload

---
+++ to.js
@@ -0,0 +1,50 @@
+const todoList = [{
+  name:
+    'make dinner',
+  dueDate: '2022-12-22'
+}, {
+  name:
+    'wash dishes',
+  dueDate: '2012-12-22'
+}];
+
+renderTodoList();
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    const { name, dueDate } = todoObject;
+    const html = `<div>
+    ${name} </div>
+    <div> ${dueDate} </div> 
+    <button onclick = "
+    todoList.splice(${i}, 1);
+    renderTodoList();
+    ">Delete</button>`;
+    todoListHTML += html;
+  }
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+let buttonAdd = document.getElementById('js-add');
+buttonAdd.addEventListener('click', addTodo);
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+
+  todoList.push({
+    name,
+    dueDate
+  });
+
+  inputElement.value = '';
+
+  renderTodoList();
+
+}
